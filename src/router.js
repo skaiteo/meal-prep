6bfb2d3c6c17f3c949c7c3e130e830b2db2ae02a
Sheng Kai Teo <skaiteo98@gmail.com>
2019-05-21 19:49:28 +0800

    Revert "Reversion"
    This reverts commit 58dd7d9cfff04556da7f206deac120f6796a8694.
    
    Yeah massive screw up here

--- src/router.js
+++ src/router.js
@@ -1,26 +1,60 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home.vue';
+import store from '@/store.js'
 
 Vue.use(Router);
 
-export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+            meta: {
+                authRequired: true
+            }
+        },
+        {
+            path: '/menu',
+            name: 'menu',
+            component: () => import('./views/Menu.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('./views/Login.vue')
+        },
+        {
+            path: '/join',
+            name: 'join',
+            component: () => import('./views/Join.vue')
+        },
+    ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.authRequired)) {
+        if (!store.state.user) {
+            next({
+                path: '/login'
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
