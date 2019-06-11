5d515fc3db0ffd07c0bca5bef6acd43c074635a8
Sheng Kai Teo <skaiteo98@gmail.com>
2019-06-11 15:47:38 +0800

    Added scrolling to button

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -1,14 +1,14 @@
 <template>
     <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
         <v-layout justify-center align-center column pa-5>
-            <div class="display-4 font-weight-bold white--text text-xs-center">HEALTHY MEALS</div>
-            <div class="display-4 font-weight-bold white--text text-xs-center mb-3">FOR YOUR TABLE</div>
+            <div class="display-3 font-weight-bold white--text text-xs-center">HEALTHY MEALS</div>
+            <div class="display-3 font-weight-bold white--text text-xs-center mb-3">FOR YOUR TABLE</div>
 
-            <div class="display-1 font-weight-medium white--text text-xs-center">
+            <div class="headline font-weight-regular white--text text-xs-center">
                 Finally be a foodie at home with fresh, chef-prepared meals delivered daily to your door.
             </div>
 
-            <v-btn fab class="mt-5 brown darken-3">
+            <v-btn fab class="mt-5 brown darken-3" @click="goToDetails">
                 <v-icon large color="white">expand_more</v-icon>
             </v-btn>
         </v-layout>
@@ -17,7 +17,17 @@
 
 <script>
 export default {
-    name: 'HomeHero'
+    name: 'HomeHero',
+
+    methods: {
+        goToDetails() {
+            this.$vuetify.goTo('#home-details', {
+                offset: 50,
+                easing: 'easeInOutCubic',
+                duration: 700
+            });
+        }
+    },
 }
 </script>
 
@@ -28,7 +38,7 @@ export default {
             rgba(0, 0, 0, 0), 
             rgba(0, 0, 0, 0.5) 
         ), 
-        url('https://source.unsplash.com/y3aP9oo9Pjc'); /* 0BhSKStVtdM, y3aP9oo9Pjc */
+        url('../assets/home.jpg'); /* 0BhSKStVtdM, y3aP9oo9Pjc */
     background-size: cover;
     height: 100%;
     width: 100%;
