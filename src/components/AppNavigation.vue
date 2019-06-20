25dbcb1851bfed8089531e3e37820a27d29adc57
Sheng Kai Teo <skaiteo98@gmail.com>
2019-06-20 13:54:51 +0800

    Updated navigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,15 +1,25 @@
 <template>
     <span>
         <!-- Side nav bar -->
-        <v-navigation-drawer app dark v-model="drawer" class="brown lighten-2" disable-resize-watcher>
+        <v-navigation-drawer
+            app
+            dark
+            v-model="drawer"
+            class="brown lighten-2"
+            disable-resize-watcher
+        >
             <v-list>
                 <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content>
-                            {{ item.title }}
-                        </v-list-tile-content>
+                    <v-list-tile :key="index" @click="navigateTo(index)">
+                        <v-list-tile-content>{{ item.title }}</v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+                <template v-if="isAuthenticated">
+                    <v-list-tile @click="logout">
+                        <v-list-tile-content>{{ 'Log Out' }}</v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider></v-divider>
                 </template>
             </v-list>
         </v-navigation-drawer>
@@ -24,13 +34,13 @@
             <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
             <div v-if="isAuthenticated">
-                <v-btn flat to="/about">{{ username }}</v-btn>
-                <v-btn outline color="white" @click="logout">LOG OUT</v-btn>
+                <v-btn flat to="/about" class="hidden-sm-and-down">{{ username }}</v-btn>
+                <v-btn outline class="hidden-sm-and-down" color="white" @click="logout">LOG OUT</v-btn>
             </div>
             <div v-else class="hidden-sm-and-down">
                 <!-- <v-btn outline @click="logInCharlie">Log In as Charlie</v-btn> -->
                 <v-btn flat to="/login">LOG IN</v-btn>
-                <v-btn color="brown darken-1" to="/join">JOIN</v-btn>
+                <v-btn color="brown darken-1" to="/signup">SIGN UP</v-btn>
             </div>
         </v-toolbar>
     </span>
@@ -39,18 +49,12 @@
 <script>
 export default {
     name: 'AppNavigation',
-    
+
     data() {
         return {
             appTitle: 'Meal Prep',
-            drawer: false,
-            items: [
-                { title: "Menu" },
-                { title: "Profile" },
-                { title: "Log In" },
-                { title: "Join" },
-            ]
-        }
+            drawer: false
+        };
     },
 
     computed: {
@@ -60,6 +64,22 @@ export default {
 
         username() {
             return this.$store.getters.username;
+        },
+
+        items() {
+            if (this.isAuthenticated) {
+                return [
+                    { title: 'Menu', to: '/menu' },
+                    { title: 'Profile', to: '/about' }
+                ];
+            } else {
+                return [
+                    { title: 'Menu', to: '/menu' },
+                    { title: 'Profile', to: '/about' },
+                    { title: 'Log In', to: '/login' },
+                    { title: 'Sign Up', to: '/signup' }
+                ];
+            }
         }
     },
 
@@ -71,9 +91,12 @@ export default {
             this.$store.dispatch('userLogin', {
                 email: 'charliemhz@email.com',
                 password: 'secret'
-            })
+            });
+        },
+        navigateTo(index) {
+            this.$router.push(this.items[index].to);
         }
-    },
+    }
 };
 </script>
 
